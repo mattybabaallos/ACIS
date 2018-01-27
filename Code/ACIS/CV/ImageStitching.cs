cf22a696f8640737a5e884aed42712ba955aa024
mattybabaallos <matty.babaallos@gmail.com>
2018-01-27 03:02:52 -0800

    Recreated sln and projects. Started looking into image stitching.

---
+++ Code/ACIS/CV/ImageStitching.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Stitching;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using System.IO;
+
+namespace CV
+{
+    public class ImageStitching
+    {
+        public void stitich()
+        {
+            var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\TestImages\";
+            var rightImagePath = path + "right.jpg";
+            var leftImagePath = path + "left.jpg";
+
+            var images = new VectorOfMat();
+            images.Push(CvInvoke.Imread(leftImagePath, ImreadModes.AnyColor));
+            images.Push(CvInvoke.Imread(rightImagePath, ImreadModes.AnyColor));
+
+
+            var result = new Mat();
+            var stitcher = new Stitcher(Stitcher.Mode.Scans, true);
+            Stitcher.Status stitchStatus = stitcher.Stitch(images, result);
+            CvInvoke.Imwrite(path + "new_image.jpg", result);
+            Console.WriteLine(stitchStatus);
+
+        }
+    }
+}
---
+++ Code/ACIS/DevConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV;
+
+namespace DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sititcher = new ImageStitching();
+            sititcher.stitich();
+            Console.ReadLine();
+        }
+    }
+}
