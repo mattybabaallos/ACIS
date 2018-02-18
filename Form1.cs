2dfd6e22a3331ed5f9efaccc9c331c50e4c3404c
Thao Tran <thao23@pdx.edu>
2018-02-17 21:01:49 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoControl
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort myport;
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void On_btn_Click(object sender, EventArgs e)
+        {
+            myport.WriteLine("A");
+
+            On_btn.Enabled = false;
+            Off_btn.Enabled = true;
+
+        }
+
+        private void init()
+        {
+            try
+            {
+                myport = new SerialPort();
+                myport.BaudRate = 9600;
+                myport.PortName = "COM4";
+                myport.Open();    
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!!", "Error");
+            }
+
+            On_btn.Enabled = true;
+            Off_btn.Enabled = false;
+        }
+
+        private void Off_btn_Click(object sender, EventArgs e)
+        {
+            myport.WriteLine("B");
+
+            On_btn.Enabled = true;
+            Off_btn.Enabled = false;
+
+        }
+    }
+}
---
+++ HelloArduino.ino
@@ -0,0 +1,15 @@
+int LED = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  char data = Serial.read();
+
+  switch (data){
+    case 'A': digitalWrite(LED, HIGH); break;
+    case 'B': digitalWrite(LED, LOW);break;
+  }
+}
