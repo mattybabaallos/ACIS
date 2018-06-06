a847210e0330ec2c900b2b89d17d675996e7c299
Unknown <matty.babaallos@gmail.com>
2018-06-06 14:44:45 -0700

    dev test

--- Code/ACIS/Dev/Program.cs
+++ Code/ACIS/Dev/Program.cs
@@ -1,20 +1,69 @@
-﻿using Services;
+﻿using Emgu.CV;
+using Emgu.CV.ML;
+using Services;
 using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Threading;
 
 namespace Dev
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            var cam1T = new Thread(() => {
+                var frame1 = new Mat();
+                var cam1 = new VideoCapture(0);
+                cam1.Start();
+                while (true)
+                {
+                    cam1.Read(frame1);
+                    CvInvoke.Imshow("Cam1", frame1);
+                    CvInvoke.WaitKey(1);
+                }
+                CvInvoke.DestroyAllWindows();
+            });
 
+            var cam2T = new Thread(() => {
+                var frame1 = new Mat();
+                var cam1 = new VideoCapture(1);
+                cam1.Start();
+                while (true)
+                {
+                    cam1.Read(frame1);
+                    CvInvoke.Imshow("Cam2", frame1);
+                    CvInvoke.WaitKey(1);
+                }
+                CvInvoke.DestroyAllWindows();
+            });
 
-            static void Main(string[] args)
-            {
-  ;
-            }
+            var cam3T = new Thread(() => {
+                var frame1 = new Mat();
+                var cam1 = new VideoCapture(2);
+                cam1.Start();
+                while (true)
+                {
+                    cam1.Read(frame1);
+                    CvInvoke.Imshow("Cam3", frame1);
+                    CvInvoke.WaitKey(1);
+                }
+            });
+
+            cam1T.Start();
+            Thread.Sleep(2000);
+     //       cam1T.Abort();
+            cam2T.Start();
+            Thread.Sleep(2000);
+       //     cam2T.Abort();
+            cam3T.Start();
+
         }
     }
+}
